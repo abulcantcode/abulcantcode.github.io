03596f07c036fef955e630f53542bb706dd131c0
Abul Chowdhury <abulchwdhury@gmail.com>
2023-10-27 17:03:53 +0100

    fix dayNight

--- app/_components/DayNight.tsx
+++ app/_components/DayNight.tsx
@@ -5,7 +5,7 @@ const DayNight = () => {
   const { setTheme, theme } = useTheme();
 
   return (
-    <label className='cursor-pointer'>
+    <label className='cursor-pointer rounded-full overflow-hidden'>
       <input
         className='hidden'
         type='checkbox'
@@ -20,13 +20,13 @@ const DayNight = () => {
       >
         <div
           className={classNames(
-            "dark:opacity-0 bg-gradient-to-b from-blue-700 to-blue-300 w-20 h-20 absolute m-auto",
+            "dark:opacity-0 bg-gradient-to-b from-blue-700 to-blue-300 w-10 h-10 absolute m-auto",
             "rounded-full overflow-hidden duration-500 dark:translate-x-10 dark:-translate-y-10"
           )}
         ></div>
         <div
           className={classNames(
-            "absolute overflow-hidden rounded-full bg-gradient-to-t from-red-700 to-pink-300 w-20 h-20 duration-500 -translate-x-10 translate-y-10 dark:translate-x-0 dark:translate-y-0 opacity-0 dark:opacity-100"
+            "absolute overflow-hidden rounded-full bg-gradient-to-t from-red-700 to-pink-300 w-10 h-10 duration-500 -translate-x-10 translate-y-10 dark:translate-x-0 dark:translate-y-0 opacity-0 dark:opacity-100"
           )}
         ></div>
       </div>
